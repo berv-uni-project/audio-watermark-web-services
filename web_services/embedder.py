443cd6d2a41d3f2b8e845dd45c0b4c18c7eef358
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2018-04-28 09:30:01 +0700

    rearange code and temporary ignore some error
    Former-commit-id: e92026979665bd048dba4f5905cd01060036b715

--- web_services/embedder.py
+++ web_services/embedder.py
@@ -1,72 +1,78 @@
+""" Embedder Module """
+import random
+import os
 import numpy as np
 from pydub import AudioSegment
 import pywt
 import cv2
 from read_image import arnold_from_file, anti_arnold_iteration
-from scipy.io import wavfile
-from scipy.fftpack import dct, idct
-import random
-import os
 
 class Embedder:
-    def _input_image_to_coef(self, coef=None, image=None):
+    """ Embedder Class """
+    @staticmethod
+    def _input_image_to_coef(coef=None, image=None):
         j = 0
         output = []
-        for i, data in enumerate(coef):
+        for data in coef:
             if j >= 0 and j < len(image):
-                data = data+image[j]
+                data = data + image[j]
                 j = j + 1
                 output.append(data)
             else:
                 output.append(data)
         return output
 
-    def _dwt_two_level(self, data=None, mode='haar'):
-        cA1, cD1 = pywt.dwt(data, mode)
-        cA2, cD2 = pywt.dwt(cA1, mode)
-        return cA1, cD1, cA2, cD2
+    @staticmethod
+    def _dwt_two_level(data=None, mode='haar'):
+        ca1, cd1 = pywt.dwt(data, mode)
+        ca2, cd2 = pywt.dwt(ca1, mode)
+        return ca1, cd1, ca2, cd2
 
-    def _idwt_two_level(self, cD1=None, cA2=None, cD2=None, mode='haar'):
-        cA1 = pywt.idwt(cA2, cD2, mode)
-        newAudioFloat = pywt.idwt(cA1, cD1, mode)
-        newAudio = np.array(newAudioFloat).astype(int)
-        return newAudio
+    @staticmethod
+    def _idwt_two_level(cd1=None, ca2=None, cd2=None, mode='haar'):
+        ca1 = pywt.idwt(ca2, cd2, mode)
+        new_audio_float = pywt.idwt(ca1, cd1, mode)
+        new_audio = np.array(new_audio_float).astype(int)
+        return new_audio
 
-    def _key_to_integer(self, key=None):
+    @staticmethod
+    def _key_to_integer(key=None):
+        default_num = 10
         if key is not None:
-            sum = 0
-            for x in key:
-                sum = sum + ord(x)
-            random.seed(sum)
-            return random.randint(10, 30)
-        else:
-            return 10
+            val = 0
+            for charachter in key:
+                val = val + ord(charachter)
+            random.seed(val)
+            default_num = random.randint(10, 30)
+        return default_num
 
-    def _insert_data_to_frame(self, audio=None, image=None):
-        cA1, cD1, cA2, cD2 = self._dwt_two_level(audio, 'haar')
-        output = self._input_image_to_coef(cD2, image)
-        newAudio = self._idwt_two_level(cD1, cA2, output, 'haar')
-        return newAudio
+    @staticmethod
+    def _insert_data_to_frame(audio=None, image=None):
+        ca1, cd1, ca2, cd2 = Embedder._dwt_two_level(audio, 'haar') # pylint: disable=unused-variable
+        out = Embedder._input_image_to_coef(cd2, image)
+        new_audio = Embedder._idwt_two_level(cd1, ca2, out, 'haar')
+        return new_audio
 
-    def _processed_image(self, image_path=None, key=None):
+    @staticmethod
+    def _processed_image(image_path=None, key=None):
         # count round from key
-        round = self._key_to_integer(key)
+        rounds = Embedder._key_to_integer(key)
         # generate scrambled image
-        image = arnold_from_file(image_path, round)
+        image = arnold_from_file(image_path, rounds)
+        out = None
         if image is not None:
-            # cv2.imwrite('../sample/scrambled.jpg',image)
             # change to image array
-            img_array = np.reshape(image, image.shape[0]*image.shape[1])
+            img_array = np.reshape(image, image.shape[0] * image.shape[1])
             # normalize
-            return [x / 255 for x in img_array]
-        else:
-            return None
+            out = [x / 255 for x in img_array]
+        return out
 
-    def embed(self, audio_path=None, image_path=None, key=None):
+    def embed(self, audio_path=None, image_path=None, key=None): # pylint: disable=too-many-locals
+        """ Main embed """
         audio_name, audio_ext = os.path.splitext(audio_path)
-        newLocation = audio_name + '-watermarked' + audio_ext
+        new_location = audio_name + '-watermarked' + audio_ext
         img_array = self._processed_image(image_path, key)
-        if img_array is None:
+        if img_array is None: # pylint: disable=too-many-nested-blocks
             print('No Image')
             return 'No Image'
         else:
@@ -75,7 +81,7 @@ class Embedder:
                 try:
                     sounds = AudioSegment.from_file(audio_path)
                     # check audio channels, if stereo just edit left channel
-                    if (sounds.channels == 2):
+                    if sounds.channels == 2:
                         audio_array = sounds.split_to_mono()
                         left_samples = audio_array[0].get_array_of_samples()
                         right_samples = audio_array[1].get_array_of_samples()
@@ -86,112 +92,135 @@ class Embedder:
                                 left_samples.append(-128)
                                 right_samples.append(-128)
                                 i = i + 1
-                            audio_array[1] = audio_array[1]._spawn(right_samples)
-
-                        newAudioLeft = self._insert_data_to_frame(
+                            audio_array[1] = audio_array[1]._spawn(right_samples) # pylint: disable=protected-access
+                        new_audio_left = Embedder._insert_data_to_frame(
                             left_samples, img_array)
-                        new_left_channel = audio_array[0]._spawn(newAudioLeft)
-                        newAudioSound = AudioSegment.from_mono_audiosegments(
-                            new_left_channel, audio_array[1])
-                        newAudioSound.export(newLocation, format='wav')
-                        return newLocation
+                        new_left_channel = audio_array[0]._spawn(new_audio_left) # pylint: disable=protected-access
+                        new_audio_sound = AudioSegment.from_mono_audiosegments(
+                            new_left_channel,
+                            audio_array[1])
+                        new_audio_sound.export(new_location, format='wav')
+                        return new_location
                     else:
                         samples = sounds.get_array_of_samples()
-                        newAudioData = self._insert_data_to_frame(samples, img_array)
-                        newSound = sounds._spawn(newAudioData)
-                        newSound.export(newLocation, format='wav')
-                        return newLocation
-                except Exception as excep:
-                    return str(excep)   
+                        new_audio_data = self._insert_data_to_frame(
+                            samples, img_array)
+                        new_sound = sounds._spawn(new_audio_data) # pylint: disable=protected-access
+                        new_sound.export(new_location, format='wav')
+                        return new_location
+                except Exception as excep: # pylint: disable=broad-except
+                    return str(excep)
             else:
                 return 'Unsupported Format'
 
-    def extract(self, watermarked_audio=None, original_audio=None, key=None, location=None, size=0):
-        try:
-            round = self._key_to_integer(key)
+    def extract(self, watermarked_audio=None, original_audio=None, key=None, location=None, size=0): # pylint: disable=too-many-arguments,too-many-locals, too-many-branches, too-many-statements
+        """ Extract Method Main """
+        try: # pylint: disable=too-many-nested-blocks
+            rounds = self._key_to_integer(key)
             ori = AudioSegment.from_file(original_audio)
             watermark = AudioSegment.from_file(watermarked_audio)
-            if (watermark.channels == ori.channels):
-                if (watermark.channels == 2):
+            if watermark.channels == ori.channels:
+                if watermark.channels == 2:
                     ori_monos = ori.split_to_mono()
                     watermark_monos = watermark.split_to_mono()
                     left_samples_ori = ori_monos[0].get_array_of_samples()
                     left_samples_watermark = watermark_monos[0].get_array_of_samples(
                     )
-                    cA1_ori, cD1_ori = pywt.dwt(left_samples_ori, 'haar')
-                    cA2_ori, cD2_ori = pywt.dwt(cA1_ori, 'haar')
-                    cA1_water, cD1_water = pywt.dwt(left_samples_watermark, 'haar')
-                    cA2_water, cD2_water = pywt.dwt(cA1_water, 'haar')
+                    ca1_ori, cd1_ori = pywt.dwt(left_samples_ori, 'haar') # pylint: disable=unused-variable
+                    ca2_ori, cd2_ori = pywt.dwt(ca1_ori, 'haar') # pylint: disable=unused-variable
+                    ca1_water, cd1_water = pywt.dwt( # pylint: disable=unused-variable
+                        left_samples_watermark, 'haar')
+                    ca2_water, cd2_water = pywt.dwt(ca1_water, 'haar')
                     out_image = []
-                    if len(cD2_water) <= len(cD2_ori):
+                    if len(ca2_water) <= len(cd2_ori):
                         j = 0
-                        for i, content in enumerate(cD2_water):
-                            if j >= 0 and j < size*size:
-                                out_image.append(content - cD2_ori[i])
+                        for i, content in enumerate(cd2_water):
+                            if j >= 0 and j < size * size:
+                                out_image.append(content - cd2_ori[i])
                                 j = j + 1
                             else:
                                 break
                         out_image[:] = [x * 255 for x in out_image]
                     else:
                         j = 0
-                        for i, content in enumerate(cD2_ori):
-                            if j >= 0 and j < size*size:
-                                out_image.append(cD2_water[i] - content)
+                        for i, content in enumerate(cd2_ori):
+                            if j >= 0 and j < size * size:
+                                out_image.append(cd2_water[i] - content)
                                 j = j + 1
                             else:
                                 break
                         out_image[:] = [x * 255 for x in out_image]
 
-                    matrix_image = np.reshape(np.array(out_image), (size, size))
-                    # cv2.imwrite('../sample/before.jpg', matrix_image)
-                    extracted_image = anti_arnold_iteration(matrix_image,round)
-                    cv2.imwrite(location,extracted_image)
+                    matrix_image = np.reshape(
+                        np.array(out_image), (size, size))
+                    extracted_image = anti_arnold_iteration(
+                        matrix_image, rounds)
+                    cv2.imwrite(location, extracted_image) # pylint: disable=no-member
                     return location
                 else:
                     left_samples_ori = ori.get_array_of_samples()
                     left_samples_watermark = watermark.get_array_of_samples()
-                    cA1_ori, cD1_ori = pywt.dwt(left_samples_ori, 'haar')
-                    cA2_ori, cD2_ori = pywt.dwt(cA1_ori, 'haar')
-                    cA1_water, cD1_water = pywt.dwt(left_samples_watermark, 'haar')
-                    cA2_water, cD2_water = pywt.dwt(cA1_water, 'haar')
+                    ca1_ori, cd1_ori = pywt.dwt(left_samples_ori, 'haar')
+                    ca2_ori, cd2_ori = pywt.dwt(ca1_ori, 'haar')
+                    ca1_water, ca1_water = pywt.dwt(
+                        left_samples_watermark, 'haar')
+                    cd2_water, cd2_water = pywt.dwt(ca1_water, 'haar')
 
                     out_image = []
-                    if len(cD2_water) <= len(cD2_ori):
+                    if len(cd2_water) <= len(cd2_ori):
                         j = 0
-                        for i, content in enumerate(cD2_water):
-                            if j >= 0 and j < size*size:
-                                out_image.append(content - cD2_ori[i])
+                        for i, content in enumerate(cd2_water):
+                            if j >= 0 and j < size * size:
+                                out_image.append(content - cd2_ori[i])
                                 j = j + 1
                             else:
                                 break
                         out_image[:] = [x * 255 for x in out_image]
                     else:
                         j = 0
-                        for i, content in enumerate(cD2_ori):
-                            if j >= 0 and j < size*size:
-                                out_image.append(cD2_water[i] - content)
+                        for i, content in enumerate(cd2_ori):
+                            if j >= 0 and j < size * size:
+                                out_image.append(cd2_water[i] - content)
                                 j = j + 1
                             else:
                                 break
                         out_image[:] = [x * 255 for x in out_image]
 
-                    matrix_image = np.reshape(np.array(out_image), (size, size))
-                    # cv2.imwrite('../sample/before.jpg', matrix_image)
-                    extracted_image = anti_arnold_iteration(matrix_image,round)
-                    cv2.imwrite(location,extracted_image)
+                    matrix_image = np.reshape(
+                        np.array(out_image), (size, size))
+                    extracted_image = anti_arnold_iteration(
+                        matrix_image, round)
+                    cv2.imwrite(location, extracted_image) # pylint: disable=no-member
                     return location
             else:
                 return 'Not Same Audio Channels'
-        except Exception as Except:
-            return str(Except)
-            
+        except Exception as ex: # pylint: disable=broad-except
+            return str(ex)
+
 
 if __name__ == '__main__':
-    embed = Embedder()
-    out = embed.embed(audio_path='../sample/classical.wav', image_path='../sample/get-frame.png', key='HELLO')
-    # out = embed.embed(audio_path='../sample/Beat_Your_Competition.mp3', image_path='../sample/final.jpg', key='HELLO')
-    # out = embed.embed(audio_path='../sample/a2002011001-e02.wav', image_path='../sample/black.jpg', key='HELLO')
-    # out = embed.embed(audio_path='../sample/a2002011001-e02-128k.mp3', image_path='../sample/A.jpg', key='HELLO')
-    # print(out)
-    embed.extract(watermarked_audio='../sample/classical-watermarked.wav',
-                  original_audio='../sample/classical.wav', key='HELLO', location='../sample/extracted-image.jpg', size=300)
+    EMBED = Embedder()
+    OUTPUT = EMBED.embed(
+        audio_path='../sample/classical.wav',
+        image_path='../sample/get-frame.png',
+        key='HELLO')
+    """
+    out = embed.embed(
+        audio_path='../sample/Beat_Your_Competition.mp3',
+        image_path='../sample/final.jpg',
+        key='HELLO')
+    out = embed.embed(
+        audio_path='../sample/a2002011001-e02.wav',
+        image_path='../sample/black.jpg',
+        key='HELLO')
+    out = embed.embed(
+        audio_path='../sample/a2002011001-e02-128k.mp3',
+        image_path='../sample/A.jpg',
+        key='HELLO')
+    """
+    EMBED.extract(
+        watermarked_audio='../sample/classical-watermarked.wav',
+        original_audio='../sample/classical.wav',
+        key='HELLO',
+        location='../sample/extracted-image.jpg',
+        size=300)
--- web_services/tasks.py
+++ web_services/tasks.py
@@ -1,79 +1,82 @@
+""" Define All Celery Tasks """
+import sys
+import os
 from functools import wraps
-
-from audio_watermark_web_services.celeryconf import app  
+import tempfile
+from celery.utils.log import get_task_logger
+import pyrebase
+import firebase_admin
+from firebase_admin import credentials, auth
+from audio_watermark_web_services.celeryconf import app
 from .models import Embed, Extract
 from .embedder import Embedder
-import pyrebase
-import tempfile
-import firebase_admin
-from firebase_admin import credentials,auth
-import sys, os
 
-from celery.utils.log import get_task_logger
+LOGGER = get_task_logger(__name__)
 
-logger = get_task_logger(__name__)
-
-config = {
+CONFIG = {
     'apiKey': 'AIzaSyCA0Hw7fo3rQXb6zoVSr386HADe1V_VD-U',
-    'authDomain':'final-project-877fd.firebaseapp.com',
-    'databaseURL':'https://final-project-877fd.firebaseio.com',
-    'storageBucket':'final-project-877fd.appspot.com',
+    'authDomain': 'final-project-877fd.firebaseapp.com',
+    'databaseURL': 'https://final-project-877fd.firebaseio.com',
+    'storageBucket': 'final-project-877fd.appspot.com',
     'serviceAccount': '/app/web_services/final-project.json'
 }
 
 # decorator to avoid code duplication
-
-def embed_job(fn):  
+def embed_job(func):
     """Decorator that will update Job with result of the function"""
 
     # wraps will make the name and docstring of fn available for introspection
-    @wraps(fn)
+    @wraps(func)
     def wrapper(job_id, *args, **kwargs):
+        """ Wrapper to Call Function"""
         job = Embed.objects.get(id=job_id)
         job.status = 'started'
         job.save()
         try:
             # execute the function fn
-            result,uid = fn(job_id, *args, **kwargs)
+            result, uid = func(job_id, *args, **kwargs)
             # check extract or embed
             job.result = result
             job.audio_output = result
             job.user_id = uid
             job.status = 'finished'
             job.save()
-        except Exception as ex:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
+        except Exception as ex: # pylint: disable=broad-except
+            exc_type, exc_obj, exc_tb = sys.exc_info() # pylint: disable=unused-variable
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             message = '{}:{}:{}'.format(exc_type, fname, exc_tb.tb_lineno)
-            logger.info(message)
+            LOGGER.info(str(ex))
+            LOGGER.info(message)
             job.result = message
             job.status = 'failed'
             job.save()
     return wrapper
 
-def extract_job(fn):  
-    """Decorator that will update Job with result of the function"""
 
+def extract_job(fun):
+    """Decorator that will update Job with result of the function"""
     # wraps will make the name and docstring of fn available for introspection
-    @wraps(fn)
+    @wraps(fun)
     def wrapper(job_id, *args, **kwargs):
+        """ Wrapper to Call Function"""
         job = Extract.objects.get(id=job_id)
         job.status = 'started'
         job.save()
         try:
             # execute the function fn
-            result, uid = fn(job_id,*args, **kwargs)
+            result, uid = fun(job_id, *args, **kwargs)
             # check extract or embed
             job.result = result
             job.image_output = result
             job.user_id = uid
             job.status = 'finished'
             job.save()
-        except Exception as ex:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
+        except Exception as ex: # pylint: disable=broad-except
+            exc_type, exc_obj, exc_tb = sys.exc_info() # pylint: disable=unused-variable
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             message = '{}:{}:{}'.format(exc_type, fname, exc_tb.tb_lineno)
-            logger.info(message)
+            LOGGER.info(str(ex))
+            LOGGER.info(message)
             job.result = message
             job.status = 'failed'
             job.save()
@@ -84,56 +87,69 @@ def extract_job(fn):
 
 @app.task
 @embed_job
-def embed_1(job_id,image_input, audio_input, key, accessToken, method_option):
+def embed_1(job_id, image_input, audio_input, key, accessToken, method_option): # pylint: disable=invalid-name, too-many-arguments, too-many-locals
+    """Embedding Task Mode 1"""
     if method_option == 'embed_1':
-        firebase = pyrebase.initialize_app(config)
+        firebase = pyrebase.initialize_app(CONFIG)
         cred = credentials.Certificate('/app/web_services/final-project.json')
         default_app = firebase_admin.initialize_app(cred)
         storage = firebase.storage()
         decoded_token = auth.verify_id_token(accessToken, app=default_app)
         with tempfile.TemporaryDirectory(prefix='{}-'.format(job_id)) as tmpdirname:
             uid = decoded_token['uid']
-            file_audio = '{}/{}.wav'.format(tmpdirname,job_id)
-            file_image = '{}/{}.jpg'.format(tmpdirname,job_id)
+            file_audio = '{}/{}.wav'.format(tmpdirname, job_id)
+            file_image = '{}/{}.jpg'.format(tmpdirname, job_id)
             storage.child(audio_input).download(file_audio, accessToken)
             storage.child(image_input).download(file_image, accessToken)
             embbeder = Embedder()
-            finished = embbeder.embed(audio_path=file_audio, image_path=file_image, key=key)
-            target = '{}/{}-watermarked.wav'.format(uid,job_id)
+            finished = embbeder.embed(
+                audio_path=file_audio,
+                image_path=file_image,
+                key=key)
+            target = '{}/{}-watermarked.wav'.format(uid, job_id)
             storage.child(target).put(finished, accessToken)
-            return target,uid
+            return target, uid
     else:
-        return 'Unsupported Method',None
+        return 'Unsupported Method', None
+
 
 @app.task
 @extract_job
-def extract_1(job_id,watermarked_audio_input, original_audio_input, size, key, accessToken, method_option): 
+def extract_1(job_id, watermarked_audio_input, original_audio_input, size, key, accessToken, method_option): # pylint: disable=line-too-long, invalid-name, too-many-arguments, too-many-locals
+    """Extracting Mode 1"""
     if method_option == 'extract_1':
-        firebase = pyrebase.initialize_app(config)
+        firebase = pyrebase.initialize_app(CONFIG)
         cred = credentials.Certificate('/app/web_services/final-project.json')
         default_app = firebase_admin.initialize_app(cred)
         storage = firebase.storage()
         decoded_token = auth.verify_id_token(accessToken, app=default_app)
         with tempfile.TemporaryDirectory(prefix='{}-'.format(job_id)) as tmpdirname:
             uid = decoded_token['uid']
-            file_audio_watermarked = '{}/{}-watermarked.wav'.format(tmpdirname,job_id)
-            file_audio_original = '{}/{}-original.wav'.format(tmpdirname,job_id)
-            temp_loc_target = '{}/{}-extracted.jpg'.format(tmpdirname,job_id)
-            storage.child(watermarked_audio_input).download(file_audio_watermarked, accessToken)
-            storage.child(original_audio_input).download(file_audio_original, accessToken)
+            file_audio_watermarked = '{}/{}-watermarked.wav'.format(
+                tmpdirname, job_id)
+            file_audio_original = '{}/{}-original.wav'.format(
+                tmpdirname, job_id)
+            temp_loc_target = '{}/{}-extracted.jpg'.format(tmpdirname, job_id)
+            storage.child(watermarked_audio_input).download(
+                file_audio_watermarked, accessToken)
+            storage.child(original_audio_input).download(
+                file_audio_original, accessToken)
             embbeder = Embedder()
             finished = embbeder.extract(watermarked_audio=file_audio_watermarked,
-                    original_audio=file_audio_original, key=key, location=temp_loc_target, size=int(size))
-            target = '{}/{}-extracted.jpg'.format(uid,job_id)
+                                        original_audio=file_audio_original,
+                                        key=key,
+                                        location=temp_loc_target,
+                                        size=int(size))
+            target = '{}/{}-extracted.jpg'.format(uid, job_id)
             storage.child(target).put(finished, accessToken)
-            return target,uid
+            return target, uid
     else:
-        return 'Unsupported Method',None
+        return 'Unsupported Method', None
 
 
 # mapping from names to tasks
 
-TASK_MAPPING = {  
+TASK_MAPPING = {
     'embed_1': embed_1,
     'extract_1': extract_1
 }
