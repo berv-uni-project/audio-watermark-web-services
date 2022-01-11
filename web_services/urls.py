c8c4ad9e289c4a98a1eb84162b45ac05460af3b9
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-01-12 06:34:05 +0700

    migrate url

--- web_services/urls.py
+++ web_services/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url, include  
+from django.urls import include, re_path
 from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()  
+router = routers.DefaultRouter()
 # register job endpoint in the router
 router.register(r'extract', views.ExtractViewSet)
 router.register(r'embed', views.EmbedViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [  
-    url(r'^(?P<version>(v1)/)', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+urlpatterns = [
+    re_path(r'^(?P<version>(v1)/)', include(router.urls)),
+    re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
